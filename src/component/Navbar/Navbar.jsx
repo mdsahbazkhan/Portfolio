9367663ca4fc7d0bf5f1472811c5b914ab110d4e
sahbaz234 <110531834+mdsahbazkhan@users.noreply.github.com>
2024-05-17 14:58:11 +0530

    Update Navbar.jsx

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,4 +1,7 @@
 
+
+
+
 import React, { useState, useEffect, useRef } from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import Logo from '../../assets/Logo.png'; // Adjust the path to your logo
@@ -12,15 +15,20 @@ const navItems = [
 
 function DrawerAppBar() {
   const [showMenu, setShowMenu] = useState(false);
-  const [currentSection, setCurrentSection] = useState('');
+  const [currentSection, setCurrentSection] = useState('home');
+  const isClickingRef = useRef(false);
 
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
   const handleMenuClick = (menuItem) => {
+    isClickingRef.current = true;
     setCurrentSection(menuItem);
     setShowMenu(false);
+    setTimeout(() => {
+      isClickingRef.current = false;
+    }, 1000);
   };
 
   useEffect(() => {
@@ -28,13 +36,18 @@ function DrawerAppBar() {
     const observerOptions = {
       root: null,
       rootMargin: '0px',
-      threshold: 0.6
+      threshold: 0.6,
     };
 
     const observerCallback = (entries) => {
+      if (isClickingRef.current) return;
+      let debounceTimer;
       entries.forEach(entry => {
         if (entry.isIntersecting) {
-          setCurrentSection(entry.target.id);
+          clearTimeout(debounceTimer);
+          debounceTimer = setTimeout(() => {
+            setCurrentSection(entry.target.id);
+          }, 100);
         }
       });
     };
@@ -51,15 +64,16 @@ function DrawerAppBar() {
     };
   }, []);
 
+
   return (
     <div className="flex flex-col h-full">
       <header className="fixed top-0 left-0 w-full z-10 bg-gray-950 shadow-md">
-        <nav className=" p-1 flex items-center justify-between  relative mx-auto">
+        <nav className="px-2 py-2 flex items-center justify-between relative mx-auto">
           <img className="mx-auto my-2" src={Logo} alt="Logo" style={{ width: '115px' }} />
-          <div className="flex items-center md:hidden " onClick={toggleMenu}>
+          <div className="flex items-center md:hidden" onClick={toggleMenu}>
             {!showMenu ? (
               <svg
-                className="w-6 h-6 cursor-pointer bg-slate-900 border right-2 rounded-md mr-1 "
+                className="w-6 h-6 cursor-pointer bg-slate-900 border right-2 rounded-md mr-1"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -82,19 +96,17 @@ function DrawerAppBar() {
           </div>
           <ul className="hidden md:flex items-center space-x-4">
             {navItems.map((item) => (
-              currentSection !== item.id && (
-                <li
-                  key={item.id}
-                  className={`px-4 py-2 duration-200 font-extrabold cursor-pointer ${
-                    currentSection === item.id ? 'text-orange-500 underline' : 'text-white'
-                  }`}
-                  onClick={() => handleMenuClick(item.id)}
-                >
-                  <AnchorLink className="anchor-link" href={item.href} offset={50}>
-                    {item.label}
-                  </AnchorLink>
-                </li>
-              )
+              <li
+                key={item.id}
+                className={`px-4 py-2 duration-200 font-extrabold cursor-pointer ${
+                  currentSection === item.id ? 'text-red-500 ' : 'text-white'
+                }`}
+                onClick={() => handleMenuClick(item.id)}
+              >
+                <AnchorLink className="anchor-link" href={item.href} offset={50}>
+                  {item.label}
+                </AnchorLink>
+              </li>
             ))}
           </ul>
           <div
@@ -111,8 +123,7 @@ function DrawerAppBar() {
           className="fixed inset-0 bg-gray-700 bg-opacity-50 z-20 md:hidden"
           onClick={toggleMenu}
         >
-          <div className="fixed top-0 left-0 w-60 h-full  
-           bg-gray-950 shadow-md z-30">
+          <div className="fixed top-0 left-0 w-60 h-full bg-gray-950 shadow-md z-30">
             <div className="text-center" onClick={toggleMenu}>
               <img className="mx-auto my-2 p-1" src={Logo} alt="Logo" style={{ width: '115px' }} />
               <hr className="border-gray-200" />
@@ -121,7 +132,7 @@ function DrawerAppBar() {
                   <li
                     key={item.id}
                     className={`px-4 py-2 duration-200 font-extrabold cursor-pointer ${
-                      currentSection === item.id ? 'text-orange-500 underline' : 'text-white'
+                      currentSection === item.id ? 'bg-gray-500 text-blue-300' : 'text-white'
                     }`}
                     onClick={() => handleMenuClick(item.id)}
                   >
@@ -142,9 +153,9 @@ function DrawerAppBar() {
           </div>
         </div>
       )}
-      
     </div>
   );
 }
 
 export default DrawerAppBar;
+
