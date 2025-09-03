5e7e456ce51baecb62837473bc8d89edcacfdce4
sahbaz234 <110531834+mdsahbazkhan@users.noreply.github.com>
2025-09-03 16:43:13 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'media', // ← follow OS theme automatically
+   // ← follow OS theme automatically
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
